f1579faa9d2fb2f66ce68f1f09f1959cb0bc9b0e
Arman19891006 <94810141+Arman19891006@users.noreply.github.com>
2024-07-25 03:02:18 +0330

    Update setup.py

--- namizun_core/setup.py
+++ namizun_core/setup.py
@@ -1,11 +1,14 @@
-from setuptools import setup
+from setuptools import setup , find_packages
 
 setup(name='namizun_core',
       version='1.3.8',
       description='namizun main functions',
       author='MalKeMit',
       author_email='khodemalkemit@gmail.com',
-      url='https://github.com/malkemit/namizun',
+      packages=find_packages(include=['psutil==5.9.4',
+                        'redis==4.3.5',
+                        'pytz==2022.6']),
+      url='https://github.com/Arman19891006/namizun',
       setup_requires=['wheel'],
       install_requires=['psutil==5.9.4',
                         'redis==4.3.5',
