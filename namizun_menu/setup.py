8c60aabe96cc936293f49db555465b38d1476de9
Arman19891006 <94810141+Arman19891006@users.noreply.github.com>
2024-10-06 14:59:51 +0330

    Update setup.py

--- namizun_menu/setup.py
+++ namizun_menu/setup.py
@@ -7,12 +7,20 @@ setup(name='namizun_menu',
       author_email='khodemalkemit@gmail.com',
       url='https://github.com/malkemit/namizun',
       setup_requires=['wheel'],
+      [build-system]
+      requires = ["setuptools", "wheel"]
+      build-backend = "setuptools.build_meta"
+      
       [tool.setools]
       packages = [
-          { include = "database" },
-          { include = "udp" },
-          # Add other packages as needed
-      ]      
+            { include = "database" },
+            { include = "udp" },
+            { include = "time" },
+            { include = "log" },
+            { include = "network" },
+            { include = "ip" }
+      ]
+
       install_requires=['colored~=1.4.4',
                         'pyfiglet~=0.8.post1',
                         'prettytable~=3.5.0']
