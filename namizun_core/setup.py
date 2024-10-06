8ad98c784e3bf6d86ced4e5cf14859d10a46cd3a
Arman19891006 <94810141+Arman19891006@users.noreply.github.com>
2024-10-06 16:29:25 +0330

    Update setup.py

--- namizun_core/setup.py
+++ namizun_core/setup.py
@@ -1,24 +1,17 @@
-from setuptools import setup , find_packages
+from setuptools import setup, find_packages
 
-setup(name='namizun_core',
-      version='1.3.8',
-      description='namizun main functions',
-      author='MalKeMit',
-      author_email='khodemalkemit@gmail.com',
-      packages=find_packages(include=['psutil==5.9.4',
-                        'redis==4.3.5',
-                        'pytz==2022.6']),
-      [build-system]
-      requires = ["setuptools", "wheel"]
-      build-backend = "setuptools.build_meta"
-      
-      [tool.setools]
-      py_modules = ["database", "udp", "time", "log", "network", "ip"]
-
-      
-      url='https://github.com/Arman19891006/namizun',
-      setup_requires=['wheel'],
-      install_requires=['psutil==5.9.4',
-                        'redis==4.3.5',
-                        'pytz==2022.6']
-      )
+setup(
+    name='namizun_core',
+    version='1.3.8',
+    description='namizun main functions',
+    author='MalKeMit',
+    author_email='khodemalkemit@gmail.com',
+    packages=find_packages(include=['database', 'udp', 'time', 'log', 'network', 'ip']),
+    url='https://github.com/Arman19891006/namizun',
+    setup_requires=['wheel'],
+    install_requires=[
+        'psutil==5.9.4',
+        'redis==4.3.5',
+        'pytz==2022.6'
+    ],
+)
