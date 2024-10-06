3a09aab5ec861a60925d3b808557fb6bd960956c
Arman19891006 <94810141+Arman19891006@users.noreply.github.com>
2024-10-06 14:50:53 +0330

    Update setup.py

--- namizun_core/setup.py
+++ namizun_core/setup.py
@@ -8,12 +8,16 @@ setup(name='namizun_core',
       packages=find_packages(include=['psutil==5.9.4',
                         'redis==4.3.5',
                         'pytz==2022.6']),
+         [build-system]
+      requires = ["setuptools", "wheel"]
+      build-backend = "setuptools.build_meta"
+      
       [tool.setools]
-      packages = [
-          { include = "database" },
-          { include = "udp" },
-          # Add other packages as needed
-      ]
+      packages = [{ include = "database" }, { include = "udp" }, { include = "time" }, { include = "log" }, { include = "network" }, { include = "ip" }]
+      package_dir = { "" = "src" }
+
+
+      
       url='https://github.com/Arman19891006/namizun',
       setup_requires=['wheel'],
       install_requires=['psutil==5.9.4',
